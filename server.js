75db612138df3f62d1e6255b5016ae5e58b4a796
realsep <58202232+realsep@users.noreply.github.com>
2020-01-16 20:43:15 +0700

    Update server.js

--- server.js
+++ server.js
@@ -21,19 +21,20 @@ app.get('/', function(req, res, next) {
 });
 
 app.post('/liff', function(req, res, next) {
-    var options = {
-        method: 'POST',
-        uri: 'https://gentle-crag-28693.herokuapp.com/test',
-        json: true
-    };
+    res.send("CONNECT SUCCESS!")
+//     var options = {
+//         method: 'POST',
+//         uri: 'https://gentle-crag-28693.herokuapp.com/test',
+//         json: true
+//     };
 
-    rp(options)
-        .then(function(parsedBody) {
-            res.send(parsedBody)
-        })
-        .catch(function(err) {
-            return next(err);
-        });
+//     rp(options)
+//         .then(function(parsedBody) {
+//             res.send(parsedBody)
+//         })
+//         .catch(function(err) {
+//             return next(err);
+//         });
 });
 
 app.listen(process.env.PORT || port, function() {
